00e657c68eb2850f9aa5c24b344ecefec178df8f
liushuang <tlnrf@sina.com>
2012-12-09 15:28:11 +0800

    ParkingManager extends ParkingBoy

--- src/main/com/buaa/liushuang/parking/ParkingManager.java
+++ src/main/com/buaa/liushuang/parking/ParkingManager.java
@@ -9,20 +9,8 @@ import java.util.List;
  * Time: 下午3:07
  * To change this template use File | Settings | File Templates.
  */
-public class ParkingManager {
-    private List<ParkPlace> parkPlaceList ;
-    private ParkPlaceChooser parkPlaceChooser;
+public class ParkingManager extends ParkingBoy {
     public ParkingManager(List<ParkPlace> parkPlaceList) {
-        this.parkPlaceList = parkPlaceList;
-        this.parkPlaceChooser = new HighestAvailableRateParkPlaceChooser();
-    }
-
-    public Ticket parkCar(Car car) throws NoSpaceParkingException {
-        ParkPlace parkPlace = this.parkPlaceChooser.chooseParkPlace(this.parkPlaceList);
-        if(parkPlace == null){
-            throw new NoSpaceParkingException();
-        }else{
-            return parkPlace.parkCar(car);
-        }
+        super(parkPlaceList,new HighestAvailableRateParkPlaceChooser());
     }
 }
